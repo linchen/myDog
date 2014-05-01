3dbbcfd26abe8289eae209e8447035d0202e83fc
Lin Chen <nudelfishandme@googlemail.com>
2014-05-01 21:31:44 +0200

    some changes

---
+++ src/main/java/com/mydog/core/events/Orders/OrderDetails.java
@@ -0,0 +1,7 @@
+package com.mydog.core.events.Orders;
+
+/**
+ * Created by Lin on 21.04.14.
+ */
+public class OrderDetails {
+}
---
+++ src/main/java/com/mydog/core/events/Orders/RequestOrderStatusEvent.java
@@ -0,0 +1,17 @@
+package com.mydog.core.events.Orders;
+
+import com.yummynoodlebar.core.events.RequestReadEvent;
+
+import java.util.UUID;
+
+public class RequestOrderStatusEvent extends RequestReadEvent {
+  private UUID key;
+
+  public RequestOrderStatusEvent(UUID key) {
+    this.key = key;
+  }
+
+  public UUID getKey() {
+    return key;
+  }
+}
